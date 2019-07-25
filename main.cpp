eea7515898334ac4f3b9a454fdab064c9b15b958
sdkdzq <qing.zhao@nio.com>
2019-07-25 10:28:23 +0800

    two

--- main.cpp
+++ main.cpp
@@ -154,7 +154,272 @@ bool isMatch(char * s, char * p){
         return true;
 }
 
+
+int divide(int dividend, int divisor){
+
+}
+
+int strStr(char * haystack, char * needle){
+
+    if(needle == "")
+        return 0;
+    if(haystack == "")
+        return -1;
+    char *p = needle;
+    char *q = haystack;
+    int count = 0;
+    while(*haystack != '\0' && *p != '\0')
+    {
+        if(*haystack == *p)
+        {
+            haystack++;
+            p++;
+//            if(p[0] == '\0')
+//                break;
+        }else
+        {
+            count++;
+            haystack = q + count;
+            p = needle;
+        }
+    }
+    if(*p == '\0')
+        return count;
+    else
+        return -1;
+}
+
+char num_char[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+char *intTostr(int n)
+{
+    char num[100] = {0};
+    char *p = "";
+    while(n)
+    {
+        char q = num_char[n % 10];
+
+        sprintf(num, "%c%s", q, p);
+//        printf("%s\n", p);
+        p = num;
+
+//        strcat(q, p);
+        n /= 10;
+    }
+    return num;
+}
+char * countAndSay(int n){
+    if(n == 1)
+        return "1";
+    if(n == 2)
+        return "11";
+    char *p = countAndSay(n - 1);
+    int count = 1;
+    char num[1000] = {0};
+    char *count_char = "";
+    while(p[0] != NULL && p[1] != NULL && p[0] != '\0' && p[1] != '\0')
+    {
+        if(p[0] == p[1]) {
+            p++;
+            count++;
+        }else
+        {
+            count_char = intTostr(count);
+            sprintf(num, "%s%s", num, count_char);
+            sprintf(num, "%s%c", num, p[0]);
+            p++;
+            count = 1;
+        }
+    }
+    count_char = intTostr(count);
+    sprintf(num, "%s%s", num, count_char);
+    sprintf(num, "%s%c", num, p[0]);
+//    *num += count_char + p[0];
+
+    return num;
+}
+
+int max(int x, int y)
+{
+    return x>y?x:y;
+}
+int min(int x, int y)
+{
+    return x>y?y:x;
+}
+
+int maxProduct(int* nums, int numsSize){
+    if(numsSize < 0)
+        return 0;
+    int i, ret, pos, neg, temp;
+    pos = nums[0];
+    neg = nums[0];
+    ret = nums[0];
+
+    for(i = 1; i < numsSize; i++)
+    {
+        temp = pos;
+        pos = max(nums[i], max(pos * nums[i], neg * nums[i]));
+        neg = min(nums[i], min(temp * nums[i], neg * nums[i]));
+        ret = max(pos, ret);
+    }
+
+
+    return ret;
+}
+
+int binarySearch(int *nums, int start, int end)
+{
+    if(start == end)
+        return nums[start];
+    int maxLeft;
+    int maxRight;
+    int middle = start + (end - start) / 2;
+    maxLeft = binarySearch(nums, start, middle);
+    maxRight = binarySearch(nums, middle + 1, end);
+    printf("%d-%d is %d %d-%d is %d\n", start, middle, maxLeft, middle + 1, end, maxRight);
+    int tmp = 0;
+    int maxL = nums[start];
+    for(int i = start + 1; i < middle + 1; i++)
+    {
+        maxL += nums[i];
+        maxL = max(maxL, tmp);
+    }
+
+    tmp = 0;
+    int maxR = nums[middle + 1];
+    for(int i = middle + 2; i < end; i++)
+    {
+        maxR += nums[i];
+        maxR = max(maxR, tmp);
+    }
+
+    int maxE = max(maxLeft, maxRight);
+    maxE = max(maxE, maxR);
+    maxE = max(maxE, maxL);
+    maxE = max(maxE, maxLeft + maxRight);
+    printf("%d %d  %d %d %d \n", maxL, maxR, maxLeft, maxRight, maxLeft + maxRight);
+    return maxE;
+
+}
+
+int maxSubArray(int* nums, int numsSize){
+    if(numsSize < 1)
+        return 0;
+    int neg = nums[0];
+    int pos = nums[0];
+    int i, ret, temp;
+    ret = nums[0];
+    int mm = nums[0];
+    for(i = 1; i < numsSize; i++)
+    {
+       if(ret + nums[i] > nums[i])
+       {
+           mm = max(mm, ret + nums[i]);
+           ret = ret + nums[i];
+       }else
+       {
+           mm = max(mm, ret);
+           mm = max(mm, nums[i]);
+           ret = nums[i];
+       }
+    }
+    return ret;
+//    return binarySearch(nums, 0, numsSize);
+}
+
+int isHW(char *s)
+{
+    int len = strlen(s);
+    int i = 0;
+    int j = len - 1;
+    while(s[i] == s[j] && i <= j)
+    {
+        i++;j--;
+    }
+    if(i >= j)
+        return 1;
+    else
+        return 0;
+}
+int minCut(char * s){
+    if(strlen(s) <= 1)
+        return 0;
+    int i, j;
+    int fenge;
+    int record[1000] = {0};
+    for(i = 0; i < strlen(s); i++)
+    {
+        record[i] = -1;
+    }
+
+    for(i = 1; i < strlen(s) + 1; i++)
+        for(j = 0; j < i; j++)
+        {
+            char p[100] = {0};
+            memcpy(p, s, sizeof(char) * i);
+            if(isHW(p))
+                record[i] = min(record[i], record[j] + 1);
+        }
+    return record[strlen(s) - 1];
+}
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int partition(char * s, int* returnSize, int** returnColumnSizes){
+    int len = strlen(s);
+    if(len <= 1)
+    {
+        printf("[\n");
+        printf("  [\"%s\"]\n", s);
+        printf("]\n");
+        return 1;
+    }
+    int i, j;
+    printf("[\n");
+    printf("  [");
+
+    for(i = 0; i < len; i++)
+    {
+        for(j = i; )
+    }
+
+    for(i = 1; i < len; i++)
+        for(j = 0; j < i; j++)
+        {
+            char p[100] = {0};
+            memcpy(p, s, sizeof(char) * (i - j));
+            if(isHW(p))
+
+        }
+    printf("]");
+    printf("]\n");
+}
+
+
 int main() {
+
+    printf("分割回文串\n");
+    printf("%d %d\n", isHW("adbda"), minCut("aab"));
+
+    int *returnSize;
+    int **returnColumnSizes;
+    partition("a", returnSize, returnColumnSizes);
+    return 0;
+    printf("乘积子序列和\n");
+    int nums[100] = {-2,1,-3,4,-1,2,1,-5,4};
+    printf("%d \n", maxSubArray(nums, 9));
+
+    printf("乘积最大子序列\n");
+    int nums1[100] = {-2, 3, -4};
+    printf("%d \n", maxProduct(nums1, 3));
+
+    printf("报数\n");
+    printf("%s \n", countAndSay(4));
 
     printf("最长回文子串\n");
     printf("%s \n", longestPalindrome("bcbbbbcb"));
@@ -165,6 +430,11 @@ int main() {
     printf("正则表达式匹配\n");
     printf("%d \n", isMatch("aa", "a*"));
 
+    printf("两数相除\n");
+    printf("%d \n", divide(10, 3));
+
+    printf("实现strStr（）\n");
+    printf("%d \n", strStr("aaaaa", "bba"));
     //std::cout << "Hello, World!" << std::endl;
     return 0;
 
